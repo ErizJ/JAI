65447ee7bfde06e6f138a22a15c19f805ad6edee
zhengjia <tal@taldeMacBook-Pro.local>
2026-04-02 20:44:28 +0800

    init project

---
+++ app/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"app/internal/inits"
+	_ "time/tzdata"
+
+	"github.com/mszlu521/thunder/config"
+	"github.com/mszlu521/thunder/logs"
+	"github.com/mszlu521/thunder/server"
+)
+
+func main() {
+	// 加载etc/config.yml中的配置
+	config.Init()
+	conf := config.GetConfig()
+
+	// 初始化日志
+	logs.Init(conf.Log)
+
+	// 初始化Gin服务
+	s := server.NewServer(conf)
+
+	// 初始化各个模块
+	inits.Init(s, conf)
+	logs.Info("init finish")
+	// 启动服务
+	s.Start()
+}
